9696914cd0767be4800a48694032ec0c9693ed34
mohammadreza <mohammadreza.mirzakhani.1999@gmail.com>
2025-09-16 20:00:35 +0330

    add update

--- TheRandomChat/Services/Authorization.cs
+++ TheRandomChat/Services/Authorization.cs
@@ -1,4 +1,10 @@
-﻿namespace TheRandomChat.Services
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TheRandomChat.Services
 {
     public class Authorization
     {
@@ -10,9 +16,18 @@
         /// <returns>return a JwtToken</returns>
         public string CreateJwt(string username)
         {
-            
 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT")));
+            var credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
 
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, username),
+            };
+
+            JwtSecurityToken jwt = new JwtSecurityToken("RandomChat", audience: "RandomChat", signingCredentials: credentials, expires: DateTime.UtcNow.AddMinutes(1));
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
 
         }
 
