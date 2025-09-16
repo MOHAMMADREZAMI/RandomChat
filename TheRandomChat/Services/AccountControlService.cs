7e7af951e3dab6584816be35e5b665db6a2b967e
mohammadreza <mohammadreza.mirzakhani.1999@gmail.com>
2025-09-16 20:47:48 +0330

    add update

--- TheRandomChat/Services/AccountControlService.cs
+++ TheRandomChat/Services/AccountControlService.cs
@@ -6,7 +6,7 @@ namespace TheRandomChat.Services
     public class AccountControlService
     {
 
-        ConcurrentDictionary<string,Account> Accounts = new ConcurrentDictionary<string,Account>();
+        private static ConcurrentDictionary<string,Account> Accounts = new ConcurrentDictionary<string,Account>();
 
         /// <summary>
         /// Check users that already taken or not 
@@ -21,7 +21,55 @@ namespace TheRandomChat.Services
                 return false;
         }
 
+        /// <summary>
+        /// Create User's Account with information
+        /// </summary>
+        /// <param name="username">username is one of the information of user</param>
+        /// <param name="ConnectionId">ConnectionId is one of the information of user</param>
+        /// <param name="TargetConnectionId"> TargetConnectionId is one of the information of user</param>
+        /// <param name="status"> status is one of the information of user</param>
+        public void CreateAccount(string username,string ConnectionId,string TargetConnectionId = null ,string status = "Waiting")
+        {
+            //variables
+            Account account;
+            //variables
 
+            account = new Account()
+            {
+                username = username,
+                ConnectionId = ConnectionId,
+                status = status
+            };
+
+            Accounts.TryAdd(username,account);
+        }
+
+
+        /// <summary>
+        /// Check the Users , who is waiting
+        /// </summary>
+        /// <returns>If someone is waiting , method returrns a ConnectionId of it , but nobody is waiting method returns null</returns>
+        public string IsSomeoneWaiting(string ConnectionId)
+        {
+            //variables
+            Account account;
+            //variables
+
+            for (int Check = 0; Check < Accounts.Count; Check++)
+            {
+
+                account = Accounts.ElementAt(Check).Value;
+
+                if(account.status == "Waiting")
+                {
+                    account.status = "Talking";
+                    account.TargetConnectionId = ConnectionId;
+                    return account.ConnectionId;
+                }
+            }
+
+            return null;
+        }
 
     }
 }
