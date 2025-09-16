c45d9680a23b5d972b056bcf8a575bc83ece8586
mohammadreza <mohammadreza.mirzakhani.1999@gmail.com>
2025-09-16 20:47:51 +0330

    add update

---
+++ TheRandomChat/Services/ChatService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using TheRandomChat.Moudel;
+
+namespace TheRandomChat.Services
+{
+    public class ChatService
+    {
+
+        private static ConcurrentDictionary<string, Chat> Chats = new ConcurrentDictionary<string, Chat>();
+
+        public void CreateChat(string Connectionid,string TargetConnectionId)
+        {
+            
+
+
+
+        }
+
+
+
+
+    }
+}
