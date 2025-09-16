960d6902127a9178e0fc6a89edc7538b3bd11e59
mohammadreza <mohammadreza.mirzakhani.1999@gmail.com>
2025-09-16 20:47:54 +0330

    add update

---
+++ TheRandomChat/SignalR/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using TheRandomChat.Services;
+
+namespace TheRandomChat.SignalR
+{
+    public class ChatHub : Hub
+    {
+        [Authorize]
+        public override Task OnConnectedAsync()
+        {
+            //variables
+            AccountControlService accountControl = new AccountControlService();
+            string TargetConnectionId;
+            //variables
+
+            if (Context.User.Identity.Name == null)
+                return null;
+
+            TargetConnectionId = accountControl.IsSomeoneWaiting(Context.ConnectionId);
+
+            if (TargetConnectionId != null)
+            {
+                accountControl.CreateAccount(Context.User.Identity.Name, Context.ConnectionId, TargetConnectionId, "Talking");
+
+            }
+                
+            else
+                accountControl.CreateAccount(Context.User.Identity.Name, Context.ConnectionId);
+
+                return base.OnConnectedAsync();
+        }
+
+
+
+    }
+}
