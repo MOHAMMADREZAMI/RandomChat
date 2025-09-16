d4ad25562e83b671b36ad919f72f35064a735071
mohammadreza <mohammadreza.mirzakhani.1999@gmail.com>
2025-09-16 20:14:07 +0330

    add update

---
+++ TheRandomChat/Services/AccountControlService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+using TheRandomChat.Moudel;
+
+namespace TheRandomChat.Services
+{
+    public class AccountControlService
+    {
+
+        ConcurrentDictionary<string,Account> Accounts = new ConcurrentDictionary<string,Account>();
+
+        /// <summary>
+        /// Check users that already taken or not 
+        /// </summary>
+        /// <param name="username">the username to check</param>
+        /// <returns>True is already takan , false is not</returns>
+        public bool CheckUserExists(string username)
+        {
+            if (Accounts.TryGetValue(username, out Account account))
+                return true;
+            else
+                return false;
+        }
+
+
+
+    }
+}
